d394884d40fccdc0b49b55adc784ee313c5e7c06
i15911403 <onurakca@std.iyte.edu.tr>
2020-10-27 01:12:45 +0000

    lab3

--- lab3/example3.py
+++ lab3/example3.py
@@ -1,13 +1,11 @@
 GPA = float(input("Enter your GPA : "))
 lectures = int(input("Enter your lectures : "))
 
-if GPA < 2 :
-  if lectures < 47:
-    print("Not enough number of lectures and GPA!")
-  elif lectures >= 47:
-    print("Not enough GPA!")
-elif GPA >=2 :
-    if lectures < 47:
-      print("Not enough number of lectures.")
-    elif lectures >= 47 :
-      print("GRADUATED")
+if GPA < 2 and  lectures < 47:
+   print("Not enough number of lectures and GPA!")
+elif GPA >=2 and lectures < 47:
+   print("Not enough GPA!")
+elif GPA >=2 and lectures < 47:
+    print("Not enough number of lectures.")
+else  :
+     print("GRADUATED")
