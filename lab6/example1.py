05a660002e5f4a4dbc33cb08029b982803ca132b
i15911403 <onurakca@std.iyte.edu.tr>
2020-11-23 09:31:31 +0000

    .

--- lab6/example1.py
+++ lab6/example1.py
@@ -1,8 +1,4 @@
-email_address = "ceng113@example.com"
-user_email_address = input("email address: ")
-if user_email_address == email_address :
-  print("Your email address is",user_email_address)
-elif email_address.upper()== user_email_address :
-  print("Your email address is",email_address.lower())
-else :
-  print("Try again")
+email = input("Email: ")
+email = email.lower()
+email = email.remove(".")
+print(email)
