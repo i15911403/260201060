c808fae0ae9b9e40bbfd8d9de09ca89b053b1418
i15911403 <onurakca@std.iyte.edu.tr>
2020-12-17 09:49:07 +0000

    lab2(because of deleted)

---
+++ lab2/example4.py
@@ -0,0 +1,3 @@
+Celcius = input(str("Enter a temperature: "))
+F = float(Celcius) *1.8 + 32
+print (float(F))
---
+++ lab2/example6.py
@@ -0,0 +1,4 @@
+v1 = 80
+v2 = 70
+minutes = (490-150)/(80+70)*60
+print(minutes,"minutes")
