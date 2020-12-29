d21211340e5c3456da13b697e692deafa5e433ee
i15911403 <onurakca@std.iyte.edu.tr>
2020-12-29 21:47:13 +0000

    lab_10

---
+++ lab_10/example4.py
@@ -0,0 +1,13 @@
+def pascal(n):
+    if n == 0:
+        return []
+    elif n == 1:
+        return [1]
+    else:
+        new_row = [1]
+        last_row = pascal(n-1)
+        for i in range(len(last_row)-1):
+            new_row.append(last_row[i] + last_row[i+1])
+        new_row += [1]
+    return new_row
+print(pascal(4))
